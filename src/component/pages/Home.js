1195f98a560b63ae586104860342fc1e1fcdcd3e
SAURABH YADAV <55534242+saurabh931@users.noreply.github.com>
2023-10-14 12:45:44 +0800

    home

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -8,13 +8,19 @@ import { Grid } from "@mui/material";
 import Dropdown from "react-bootstrap/Dropdown";
 
 import linkedin from "../Image/linkedin.png";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const { linkedInLogin } = useLinkedIn({
     clientId: "86vhj2q7ukf83q",
     redirectUri: `${window.location.origin}/linkedin`, // for Next.js, you can use `${typeof window === 'object' && window.location.origin}/linkedin`
     onSuccess: (code) => {
       console.log(code);
+      // Store the code in localStorage
+      localStorage.setItem("linkedin_code", code);
+      // Navigate to /linkedin
+      navigate("/linkedin");
     },
     onError: (error) => {
       console.log(error);
@@ -33,7 +39,10 @@ function Home() {
         <div className="t-a-c m-b-2">Monetize your professional value</div>
 
         <div className="t-a-c ">
-          <button className="b-r-40 bg_blue b-n c-w  p-x-2 p-y-0_5 "   onClick={linkedInLogin}>
+          <button
+            className="b-r-40 bg_blue b-n c-w  p-x-2 p-y-0_5 "
+            onClick={linkedInLogin}
+          >
             <img src={linkedin} className="w-2" />
             &nbsp; Signup with Linkedin
           </button>
