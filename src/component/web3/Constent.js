d4674a99aa6d19dee5911e212d73d2a6ec7545c2
Nikhil Rana <nikkrana28@gmail.com>
2023-10-14 14:19:50 +0530

    added web3 and contract

---
+++ src/component/web3/Constent.js
@@ -0,0 +1,570 @@
+export const LinkBerrySlicesABI = [
+  {
+    type: "event",
+    name: "OwnershipTransferred",
+    inputs: [
+      {
+        type: "address",
+        name: "previousOwner",
+        internalType: "address",
+        indexed: true,
+      },
+      {
+        type: "address",
+        name: "newOwner",
+        internalType: "address",
+        indexed: true,
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "Trade",
+    inputs: [
+      {
+        type: "address",
+        name: "trader",
+        internalType: "address",
+        indexed: false,
+      },
+      {
+        type: "address",
+        name: "subject",
+        internalType: "address",
+        indexed: false,
+      },
+      { type: "bool", name: "isBuy", internalType: "bool", indexed: false },
+      {
+        type: "uint256",
+        name: "sliceAmount",
+        internalType: "uint256",
+        indexed: false,
+      },
+      {
+        type: "uint256",
+        name: "ethAmount",
+        internalType: "uint256",
+        indexed: false,
+      },
+      {
+        type: "uint256",
+        name: "protocolEthAmount",
+        internalType: "uint256",
+        indexed: false,
+      },
+      {
+        type: "uint256",
+        name: "subjectEthAmount",
+        internalType: "uint256",
+        indexed: false,
+      },
+      {
+        type: "uint256",
+        name: "supply",
+        internalType: "uint256",
+        indexed: false,
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "function",
+    stateMutability: "payable",
+    outputs: [],
+    name: "buySlices",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "calculateSliceWorth",
+    inputs: [
+      { type: "uint256", name: "_days", internalType: "uint256" },
+      { type: "uint256", name: "_connections", internalType: "uint256" },
+      { type: "uint256", name: "_followers", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "getBuyPriceAfterFee",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+      { type: "uint256", name: "_days", internalType: "uint256" },
+      { type: "uint256", name: "_connections", internalType: "uint256" },
+      { type: "uint256", name: "_followers", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "getBuyPricePerSlice",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+      { type: "uint256", name: "_days", internalType: "uint256" },
+      { type: "uint256", name: "_connections", internalType: "uint256" },
+      { type: "uint256", name: "_followers", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "pure",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "getPrice",
+    inputs: [
+      { type: "uint256", name: "supply", internalType: "uint256" },
+      { type: "uint256", name: "amount", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "getSellPriceAfterFee",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+      { type: "uint256", name: "_days", internalType: "uint256" },
+      { type: "uint256", name: "_connections", internalType: "uint256" },
+      { type: "uint256", name: "_followers", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "getSellPricePerSlice",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+      { type: "uint256", name: "_days", internalType: "uint256" },
+      { type: "uint256", name: "_connections", internalType: "uint256" },
+      { type: "uint256", name: "_followers", internalType: "uint256" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "address", name: "", internalType: "address" }],
+    name: "owner",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "address", name: "", internalType: "address" }],
+    name: "protocolFeeDestination",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "protocolFeePercent",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "nonpayable",
+    outputs: [],
+    name: "renounceOwnership",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "satsPerConnection",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "satsPerDay",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "payable",
+    outputs: [],
+    name: "sellSlices",
+    inputs: [
+      { type: "address", name: "linkberryUser", internalType: "address" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "nonpayable",
+    outputs: [],
+    name: "setFeeDestination",
+    inputs: [
+      { type: "address", name: "_feeDestination", internalType: "address" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "slicesBalance",
+    inputs: [
+      { type: "address", name: "", internalType: "address" },
+      { type: "address", name: "", internalType: "address" },
+    ],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "slicesSupply",
+    inputs: [{ type: "address", name: "", internalType: "address" }],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "statsPerFollower",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "view",
+    outputs: [{ type: "uint256", name: "", internalType: "uint256" }],
+    name: "subjectFeePercent",
+    inputs: [],
+  },
+  {
+    type: "function",
+    stateMutability: "nonpayable",
+    outputs: [],
+    name: "transferOwnership",
+    inputs: [{ type: "address", name: "newOwner", internalType: "address" }],
+  },
+];
+export const LinkBerrySlicesAddress =
+  "0x53eb0AC3eef4037063e015Da3Fac1FDCb0E36060";
+
+export const ERC20ABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_symbol",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "burn",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export const TOKEN_GOMETA = "0xb05F82dC8C3b492220f33dD89665d6A3359164F1";
---
+++ src/component/web3/web3.js
@@ -0,0 +1,183 @@
+import Web3 from 'web3/dist/web3.min'
+import { ERC20ABI, LinkBerrySlicesABI, LinkBerrySlicesAddress, TOKEN_GOMETA } from './Constent'
+import { EthereumProvider } from '@walletconnect/ethereum-provider'
+import axios from "axios"
+
+
+// let web3 = new Web3("https://sepolia.infura.io/v3/6e1530cd10fb4631a54c14a5f07b25a6") ;
+let web3 = new Web3("https://rpc.testnet.mantle.xyz")
+var provider;
+var walletprovider;
+
+const walletConnectProv = async()=>{
+  provider = await EthereumProvider.init({
+    projectId:'b9bd638e50d4ce444f33b0499cac8433',
+    chains: [11155111],
+    showQrModal: true,
+    methods: [
+      "eth_sendTransaction",
+      "eth_signTransaction",
+      "eth_sign",
+      "personal_sign",
+      "eth_signTypedData",
+    ],
+    events: ["chainChanged", "accountsChanged"]
+  })
+  return provider
+}
+
+
+export const WalletConnect = async () => {
+  try {
+    provider = await walletConnectProv()
+    await provider.disconnect()
+    const data = await provider.enable()
+    web3 = new Web3(provider)
+    walletprovider = provider
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const DissconnectWallet = async () => {
+  provider = await walletConnectProv()
+  await provider.disconnect()
+  web3 = null
+}
+
+export const MetaMasklogin = async () => {
+  const data = await window.web3.currentProvider.enable()
+  web3 = new Web3(window.web3.currentProvider)
+  walletprovider = await window.web3.currentProvider
+  return data[0]
+}
+
+export const getUserAddress = async () => {
+  try {
+    const address = await web3.eth.getAccounts()
+    return address[0]
+  } catch (error) {
+    // console.log(error)
+  }
+}
+
+
+export const get_Metapower_Contract= async () => {
+  try {
+  const Contract = new web3.eth.Contract(LinkBerrySlicesABI, LinkBerrySlicesAddress)
+  return Contract
+  } catch (error) {
+    // console.log(error)
+  }
+}
+
+
+export const get_ERC_20_Contract= async (address) => {
+    try {
+    const Contract = new web3.eth.Contract(ERC20ABI, address)
+    return Contract
+    } catch (error) {
+      // console.log(error)
+    }
+  }
+
+
+export const get_Token_Balance = async (tokenAddress, userAddress) => {
+  try {
+    
+    const Contract = await get_ERC_20_Contract(tokenAddress)
+    const balance = await Contract.methods.balanceOf(userAddress).call();
+    const bal = await fromwie(balance)
+    return Number(bal)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const towie = async (amount) => {
+  try {
+    const number = await web3.utils.toWei(amount.toString(), 'ether')
+    return number
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fromwie = async (amount) => {
+  try {
+    const number = await web3.utils.fromWei(amount.toString(), 'ether')
+    return number
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const GetChainId = async () => {
+  try {
+    const id = await web3.eth.getChainId()
+    return id
+  } catch (error) {
+
+  }
+}
+
+
+export const userBalanceOfChain = async (userAddress) => {
+  try {
+     const data = await axios.post('https://rpc.testnet.mantle.xyz', {
+      "jsonrpc": "2.0",
+      "method": "eth_getBalance",
+      "params": [userAddress, "latest"],
+      "id": 1
+    }).then((res) => {
+      const bal = (parseInt(res.data.result, 16)/10**18)
+      console.log("Native balance", bal)
+      return Number(bal)
+    }).catch((error) => {
+      console.log(error)
+    })
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export const convertToLowercase =(str)=> {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      result += String.fromCharCode(charCode + 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
+
+
+// export const Approve = async () => {
+//   try {
+//     const contract = await get_ERC_20_Contract(TOKEN_GOMETA)
+//   const data = await contract.methods.approve(MetaPowerADDRESS,115792089237316195423570985008687907853269984665640564039457584007913129639935n,).send({ from: await getUserAddress() })
+//   return data
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// export const Allow = async(owner)=>{
+//   try {
+//     const contract = await get_ERC_20_Contract(TOKEN_GOMETA)
+//     const data = await contract.methods.allowance(owner, MetaPowerADDRESS).call();
+//     return data;
+//   } catch (error) {
+//     // console.log(error)
+//   }
+// }
+
+export const mainprovider = ()=>{
+  return walletprovider
+}
+
