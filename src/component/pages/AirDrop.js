cdbf9e7835dd259b74eccd8d964c7921c937a083
Nikhil Rana <nikkrana28@gmail.com>
2023-10-15 06:57:03 +0530

    fetch invite codes

--- src/component/pages/AirDrop.js
+++ src/component/pages/AirDrop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import Tab from "react-bootstrap/Tab";
 import blue_circle from "../Image/blue_circle.png";
@@ -6,18 +6,40 @@ import Tabs from "react-bootstrap/Tabs";
 import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import { AiOutlineCopy } from "react-icons/ai";
+import axios from "axios";
 
-function Link_berry() {
+function Link_berry({ url }) {
   const [value, setValue] = React.useState("one");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const [userAddrees, setUserAddress] = useState();
+  const [follower, setFollower] = useState();
+  const [collections, setCollections] = useState();
+  const [days, setDays] = useState();
+  const [codes, setCode] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      const myusername = window.localStorage.getItem("username");
+      axios
+        .post(`${url}/user/getuser`, {
+          username: myusername,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setCode(res.data.inviteCodes);
+        });
+    };
+    init();
+  }, []);
+
   return (
     <div className="p-t-2 ">
       <Grid container>
         <Grid item xs={12} sm={12} md={3} lg={2}>
-          <Sidebar />
+          <Sidebar url={url} />
         </Grid>
         <Grid item xs={12} sm={12} md={9} lg={10} className="pa-2">
           <div className="f-s-1_5 f-w-600  m-b-1">
@@ -30,32 +52,27 @@ function Link_berry() {
             friend.tech
           </div>
           <div className="m-t-1">Your points</div>
-          <div className="m-t-1 f-w-600 f-s-2">8</div>
+          <div className="m-t-1 f-w-600 f-s-2">1</div>
           <div className="m-t-3">Invite a friend</div>
-          <div className="m-y-2 d-f a-i-c">
-            <div className="m-r-7">ft-66gzzz8n</div>
-            <div className="">
-              <AiOutlineCopy />
-            </div>
-          </div>
-          <div className="m-y-2 d-f a-i-c">
-            <div className="m-r-7">ft-66gzzz8n</div>
-            <div className="">
-              <AiOutlineCopy />
-            </div>
-          </div>
-          <div className="m-y-2 d-f a-i-c">
-            <div className="m-r-7">ft-66gzzz8n</div>
-            <div className="">
-              <AiOutlineCopy />
-            </div>
-          </div>
-          <div className="m-y-2 d-f a-i-c">
-            <div className="m-r-7">ft-66gzzz8n</div>
-            <div className="">
-              <AiOutlineCopy />
-            </div>
-          </div>
+
+          {codes &&
+            codes.map((res) => {
+              return (
+                <div className="m-y-2 d-f a-i-c">
+                  <div className="m-r-7">{res}</div>
+                  <div
+                    className=""
+                    onClick={() =>
+                      navigator.clipboard.writeText(res).then(() => {
+                        alert("copied");
+                      })
+                    }
+                  >
+                    <AiOutlineCopy />
+                  </div>
+                </div>
+              );
+            })}
         </Grid>
       </Grid>
     </div>
